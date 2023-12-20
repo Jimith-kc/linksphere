d0485c48a3e4376e9ab3576be59876672f9049c0
jimith <jimithkc1998@gmail.com>
2023-12-20 10:25:37 +0530

    initial commit

---
+++ social/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from social.models import Stories,Posts
+
+# Register your models here.
+
+admin.site.register(Stories)
+admin.site.register(Posts)
+
+
+#python manage.py createsuperuser
